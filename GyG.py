b47df9cee2b0265ba3953e65cb5d6de487deac96
King-hus <bdaljawydakrmy@gmail.com>
2024-09-20 16:45:09 +0430

    GyG.py
    pkg install python
    pkg install git
    Git clone https://github.com/King-hus/GyG.git
    ls
    Cd GyG
    ls
    Python GyG.py

---
+++ GyG.py
@@ -0,0 +1,25 @@
+2024-02-06 16:26:40+0430 | SHUTDOWN |       | REASON: userrequested
+2https://github.com/King-hus/GyG.git
+024-02-08 09:18:50+0430 |    ON    | NP    | A325NKSU3DWE3 [-]
+2024-02-08 09:39:24+0430 | SHUTDOWN |       | REASON: userrequested
+2024-02-08 14:43:43+0430 |    ON    | NP    | A325NKSU3DWE3 [-]
+2024-05-16 23:04:07+0430 | SHUTDOWN |       | REASON: no power
+2024-05-16 18:41:21+0000 |          | RP    |  /  / 3514000(0%) 
+2024-05-16 18:41:21+0000 |   LPM    |
+2024-05-17 07:58:42+0430 |    ON    | RP    | A325NKSU3DWE3 [-]
+2024-06-04 13:25:03+0430 |    ON    | MP    | A325NKSU3DWE3 [-]
+2024-06-05 08:35:43+0430 |  REBOOT  |       | REASON: shell
+2024-06-05 08:37:10+0430 |    ON    | RP    | A325NKSU3DWE3 [-]
+2024-06-16 14:35:20+0430 | SHUTDOWN |       | REASON: userrequested
+2024-06-16 16:52:04+0430 |    ON    | NP    | A325NKSU3DWE3 [-]
+2024-07-29 20:07:28+0430 |    ON    | MP    | A325NKSU3DWE3 [-]
+2024-08-09 19:08:06+0430 | SHUTDOWN |       | REASON: no power
+2024-08-10 02:28:01+0000 |          | RP    |  /  / 4328000(100%) 
+2024-08-10 02:28:01+0000 |   LPM    |
+2024-08-10 08:53:34+0430 |    ON    | NP    | A325NKSU3DWE3 [-]
+2024-08-14 23:01:03+0430 | SHUTDOWN |       | REASON: userrequested
+2024-08-15 01:25:52+0430 |    ON    | NP    | A325NKSU3DWE3 [-]
+2024-08-16 21:35:05+0430 | SHUTDOWN |       | REASON: userrequested
+2024-08-16 23:57:51+0000 |          | RP    |  /  / 3635000(4%) 
+2024-08-16 23:57:51+0000 |   LPM    |
+2024-08-17 13:42:34+0430 |    ON    | NP    | A325NKSU3DWE3 [-]
